2782e05633481302d3613abdb94034c1b6ca0255
KC Thomas <36351704+kacesera@users.noreply.github.com>
2020-11-27 14:37:37 +0000

    Update cors.rb
    Testing to see if removing one allow block will allow it to be run on our host domain

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,11 +6,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     # credentials: true
   end
 
-  allow do
-    origins "http://localhost:3001"
-    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    # credentials: true
-  end
+#   allow do
+#     origins "http://localhost:3001"
+#     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+#     # credentials: true
+#   end
    
   allow do
     origins "http://nonstop-growth.surge.sh/"
